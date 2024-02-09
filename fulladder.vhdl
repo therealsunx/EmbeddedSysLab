1fbc06524806a64b4bba01965522de550169c375
SunX-1224 <sunilsapkota1224@gmail.com>
2024-02-09 11:44:48 +0545

    sequence 1011 detector added

--- fulladder.vhdl
+++ fulladder.vhdl
@@ -1,15 +1,16 @@
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
  
-entity full_adder_vhdl_code is
- Port ( A : in STD_LOGIC;
- B : in STD_LOGIC;
- Cin : in STD_LOGIC;
- S : out STD_LOGIC;
- Cout : out STD_LOGIC);
-end full_adder_vhdl_code;
+entity full_adder is Port (
+    A : in STD_LOGIC;
+    B : in STD_LOGIC;
+    Cin : in STD_LOGIC;
+    S : out STD_LOGIC;
+    Cout : out STD_LOGIC
+);
+end full_adder;
  
-architecture gate_level of full_adder_vhdl_code is
+architecture gate_level of full_adder is
  
 begin
  
