c38458d0a036b078af315ec343e555edf76dc00b
SunX-1224 <sunilsapkota1224@gmail.com>
2024-02-09 19:19:09 +0545

    mux added 2:1

---
+++ mux2x1.vhdl
@@ -0,0 +1,24 @@
+library ieee;
+use ieee.std_logic_1164.all;
+
+entity mux is Port(
+    a0, a1, s, en : in std_logic;
+    q : out std_logic;
+);
+end mux;
+
+architecture behaviour of mux is
+    begin
+        process(en)
+        begin
+            if en is '1' then
+                if s is '0' then
+                    q <= a0;
+                else
+                    q <= a1;
+                end if;
+            else
+                q <= '0';
+            end if;
+        end process;
+end behaviour;
